799388968e4bcc1470de9a7813acb969a7992ce0
Giordon Stark <kratsg@gmail.com>
2020-08-08 12:42:12 -0400

    update and use CI to test things

--- mg5creator.py
+++ mg5creator.py
@@ -1,105 +1,103 @@
 import argparse
-import os
 import shutil
+from string import Template
+from pathlib import Path
+import pprint
 
-parser = argparse.ArgumentParser(description='Process some arguments.')
+parser = argparse.ArgumentParser(description="Process some arguments.")
 
 # Cards for the MadGraph run
-parser.add_argument('-r', '--run'    , default="RunCards/default_LO.dat",      help='path to MG5 run card')
-parser.add_argument('-P', '--proc'   ,                                         help='path to MG5 proc card')
-parser.add_argument('-p', '--param'  , default="ParamCards/Higgsino",          help='path to SLHA/param card')
-parser.add_argument('-y', '--pythia' , default="PythiaCards/pythia8_card.dat", help='path to pythia card')
+parser.add_argument(
+    "-r", "--run", default="RunCards/default_LO.dat", help="path to MG5 run card"
+)
+parser.add_argument("-P", "--proc", help="path to MG5 proc card")
+parser.add_argument(
+    "-p", "--param", default="ParamCards/Higgsino.slha", help="path to SLHA/param card"
+)
+parser.add_argument(
+    "-y", "--pythia", default="PythiaCards/pythia8_card.dat", help="path to pythia card"
+)
 
 # Options for customizing the run
-parser.add_argument('-m', '--mass'   , action='append', nargs=2,               help="pass in like '-m MN1 150'")
-parser.add_argument('-E', '--sqrts'  , default=13,                             help="Center of mass energy, in GeV")
-parser.add_argument('-n', '--numev'  , default=10000,                          help="Number of events to process")
+parser.add_argument(
+    "-m", "--mass", action="append", nargs=2, help="pass in like '-m MN1 150'"
+)
+parser.add_argument(
+    "-E", "--sqrts", default=13000.0, help="Center of mass energy, in GeV"
+)
+parser.add_argument("-n", "--numev", default=10000, help="Number of events to process")
 
 # Tag for this run
-parser.add_argument('-t', '--tag'    , default="run",                          help='name for the job')
+parser.add_argument("-t", "--tag", default="run", help="name for the job")
 
 # Random seed for this job
-parser.add_argument('-s', '--seed'   , default=0,                              help='random seed')
+parser.add_argument("-s", "--seed", default=0, help="random seed")
 
+parser.add_argument(
+    "-o", "--output", default="output", help="output directory for generated files"
+)
 
 args = parser.parse_args()
 
-
-# create a new directory for this run
+# Ensure directory exists
+output_path = Path(args.output).joinpath(args.tag).resolve()
 try:
-    os.mkdir("output")
+    output_path.mkdir(parents=True, exist_ok=False)
 except FileExistsError:
-    pass
-try:
-    os.mkdir("output/%s"%args.tag)
-except FileExistsError:
-    print("Output name %s already used, pick another." % args.tag)
+    print(f"{args.tag} is already used, pick another tag or delete the directory.")
     exit(1)
 
-# ===========================================================================================================================
-#
+substitution = dict(
+    ecms=float(args.sqrts) / 2, nevents=int(args.numev), iseed=int(args.seed)
+)
+
+for particle, mass in args.mass:
+    if particle in substitution:
+        raise KeyError(f"{particle} cannot be redefined.")
+    substitution[particle] = float(mass)
+
+pprint.pprint(substitution)
+
 # Update the param card
-#
-newparamcard="output/"+args.tag+"/"+args.param.split("/")[-1]
-newparamcardf=open(newparamcard,"w")
-for line in open(args.param):
-    written=False
-    for part,mass in args.mass:
-        if part in line:
-            newparamcardf.write(line.replace(part,mass))
-            written=True
-            break
-    if not written:
-        newparamcardf.write(line)
-newparamcardf.close()
+param_card_path = Path(args.param).resolve()
+new_param_card_path = output_path.joinpath(param_card_path.name)
 
-#
+new_param_card_path.write_text(
+    Template(param_card_path.read_text()).substitute(substitution)
+)
+
 # Update the run card
-#
-newruncard="output/"+args.tag+"/"+args.run.split("/")[-1]
-newruncardf=open(newruncard,"w")
-for line in open(args.run):
-    if "ecms" in line:
-        newruncardf.write(line.replace("ecms",str(float(args.sqrts)/2)))
-    elif "nevents" in line:
-        sline=line.split()
-        sline[0]=args.numev
-        newruncardf.write(' '.join(sline)+"\n")
-    else:
-        newruncardf.write(line)
-newruncardf.close()
+run_card_path = Path(args.run).resolve()
+new_run_card_path = output_path.joinpath(run_card_path.name)
 
-#
+new_run_card_path.write_text(
+    Template(run_card_path.read_text()).substitute(substitution)
+)
+
 # Copy the proc card
-#
-shutil.copyfile(args.proc,"output/"+args.tag+"/"+args.proc.split("/")[-1])
-# ===========================================================================================================================
+proc_card_path = Path(args.proc).resolve()
+new_proc_card_path = output_path.joinpath(proc_card_path.name)
+shutil.copyfile(proc_card_path, new_proc_card_path)
 
-
-# ===========================================================================================================================
-#
 # Create the madgraph configuration card
-#
-mg5config=open("output/"+args.tag+"/"+"run.mg5","w")
 
-for line in open(args.proc):
-    if line.strip()!="output" and line.strip()!="output -f":
-        mg5config.write(line)
-    else:
-        mg5config.write("output PROC_%s\n" % args.tag)
-    
+# set run_mode 0   # mg5_configuration.txt
+config = f"""
+launch PROC_{args.tag}
+shower=Pythia8
+madspin=OFF
+reweight=OFF
+{new_param_card_path.name}
+{new_run_card_path.name}
+set iseed {args.seed}
+done
+"""
 
-#mg5config.write("set run_mode 0\n")  # mg5_configuration.txt
-mg5config.write("launch PROC_%s\n" % args.tag)
-mg5config.write("shower=Pythia8\n")
-mg5config.write("madspin=OFF\n")
-mg5config.write("reweight=OFF\n")
-mg5config.write(newparamcard.split("/")[-1]+"\n")
-mg5config.write(newruncard.split("/")[-1]+"\n")
-mg5config.write("set iseed %d\n" % args.seed)
-mg5config.write("done\n")
-    
-mg5config.close()
-# ===========================================================================================================================
-
-
+with output_path.joinpath("run.mg5").open(mode="w") as mg5config:
+    for proc_line in new_proc_card_path.open():
+        if not proc_line.strip():
+            continue
+        if proc_line.startswith("output"):
+            proc_line = f"output PROC_{args.tag}\n"
+        mg5config.write(proc_line)
+    mg5config.write(config)
