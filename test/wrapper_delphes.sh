26cc467816a3e92eca7ffec68b550f8d3d97429c
Mike Hance <michael.hance@cern.ch>
2020-08-11 20:01:42 -0700

    change tag to master

--- test/wrapper_delphes.sh
+++ test/wrapper_delphes.sh
@@ -12,7 +12,7 @@ docker run \
        -v ${base}/cards:/cards \
        -v ${base}/${datadir}:/data \
        -w /output \
-       gitlab-registry.cern.ch/scipp/mario-mapyde/delphes \
+       gitlab-registry.cern.ch/scipp/mario-mapyde/delphes:master \
        'cp $(find /data/ -name "*hepmc.gz") hepmc.gz && \
         gunzip hepmc.gz && \
         /usr/local/share/delphes/delphes/DelphesHepMC /cards/delphes/delphes_card_ATLAS.tcl delphes.root hepmc && \
@@ -30,7 +30,7 @@ docker run \
        -v ${base}/scripts:/scripts \
        -v ${base}/${datadir}:/data \
        -w /output \
-       gitlab-registry.cern.ch/scipp/mario-mapyde/delphes \
+       gitlab-registry.cern.ch/scipp/mario-mapyde/delphes:master \
        '/scripts/SimpleAna.py --input /data/delphes/delphes.root --output histograms.root && \
         rsync -rav . /data/analysis'
 
