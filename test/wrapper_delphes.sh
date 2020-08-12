7ae61635f587c1dba90b133aee9f51697ccb5a09
Mike Hance <michael.hance@cern.ch>
2020-08-11 22:38:20 -0700

    fixing variables for delphes and simpleanalysis

--- test/wrapper_delphes.sh
+++ test/wrapper_delphes.sh
@@ -2,10 +2,12 @@
 set -e # exit when any command fails
 
 tag=${1:-"test_Higgsino_001"}
-delphescard=${2:-"delphes_card_ATLAS.tcs"}
+delphescard=${2:-"delphes_card_ATLAS.tcl"}
 lumi=${3:-"1000"}
 base=${PWD}
 datadir=output/${tag}
+
+set -x
 
 docker run \
        --log-driver=journald \
@@ -14,9 +16,12 @@ docker run \
        -v ${base}/cards:/cards \
        -v ${base}/${datadir}:/data \
        -w /output \
+       --env delphescard=${delphescard} \
        gitlab-registry.cern.ch/scipp/mario-mapyde/delphes:master \
-       'cp $(find /data/ -name "*hepmc.gz") hepmc.gz && \
+       'set -x && \
+        cp $(find /data/ -name "*hepmc.gz") hepmc.gz && \
         gunzip hepmc.gz && \
+	/bin/ls -ltrh --color && \
         /usr/local/share/delphes/delphes/DelphesHepMC /cards/delphes/${delphescard} delphes.root hepmc && \
         rsync -rav --exclude hepmc . /data/delphes'
 
@@ -32,8 +37,10 @@ docker run \
        -v ${base}/scripts:/scripts \
        -v ${base}/${datadir}:/data \
        -w /output \
+       --env lumi=${lumi} \
        gitlab-registry.cern.ch/scipp/mario-mapyde/delphes:master \
-       '/scripts/SimpleAna.py --input /data/delphes/delphes.root --output histograms.root --lumi ${lumi} && \
+       'set -x && \
+        /scripts/SimpleAna.py --input /data/delphes/delphes.root --output histograms.root --lumi ${lumi} && \
         rsync -rav . /data/analysis'
 
 # dump docker logs to text file
