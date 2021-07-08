2b5035b540ff398efdcb0d724c59618e85e9619c
Mike Hance <michael.hance@cern.ch>
2021-07-08 12:40:13 -0700

    adding command line options to run_reinterp.sh

--- run_reinterp.sh
+++ run_reinterp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -e # exit when any command fails
 
-nevents=10000
+nevents=50000
 cores=8
 seed=0
 analysis="EwkCompressed2018"
@@ -14,13 +14,40 @@ mmjj=0.0
 suffix="J${ptj1min}"
 XSoverride=""
 
+clobber_mgpy=false
+clobber_delphes=false
+clobber_ana=false
+
 ecms=13
-mass=150
-masssplitting=5
+mass=125
+masssplitting=20
 
 # to run sleptons.  make sure we set "-n" in the options below.
 proc="isrslep"
 params="SleptonBino"
+
+while getopts "E:M:S:N:c:d:f:P:p:J:L:F:s:gla" opt; do
+    case "${opt}" in
+	E) ecms=$OPTARG;;
+	M) mass=$OPTARG;;
+	P) proc=$OPTARG;;
+	p) params=$OPTARG;;
+	N) nevents=$OPTARG;;
+	s) suffix=$OPTARG;;
+	c) cores=$OPTARG;;
+	g) clobber_mgpy=true;;
+	l) clobber_delphes=true;;
+	a) clobber_ana=true;;
+	J) ptj1min=$OPTARG;;
+	S) masssplitting=$OPTARG;;
+	d) seed=$OPTARG;;
+	L) delphescard=$OPTARG;;
+	f) simpleanalysis=$OPTARG;;
+	F) likelihood=$OPTARG;;
+	\?) echo "Invalid option: -$OPTARG";;
+    esac
+done
+	
 
 ./run_VBFSUSY_standalone.sh \
     -E ${ecms} \
