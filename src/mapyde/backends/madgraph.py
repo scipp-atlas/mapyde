ce787019f948f4108c32c645a1f6485858b328e9
Giordon Stark <kratsg@gmail.com>
2022-07-01 16:40:18 -0700

    add version handling

--- src/mapyde/backends/madgraph.py
+++ src/mapyde/backends/madgraph.py
@@ -11,12 +11,19 @@ import sys
 logging.basicConfig()
 log = logging.getLogger()
 
+
 def generate_mg5config(config):
-    output_path = Path(config.base['path']).joinpath(config.base['output']).resolve()
+    old_versions = ["2.4.3", "2.3.3"]
+    OLD_VERSION = False
+    if any(version in config.madgraph["version"] for version in old_versions):
+        OLD_VERSION = True
+        log.warning(f"Old madgraph version detected.")
+
+    output_path = Path(config.base["path"]).joinpath(config.base["output"]).resolve()
     output_path.mkdir(parents=True, exist_ok=True)
 
     # Ensure pythia card exists
-    _pythia_card_path = Path(config.pythia['card'])
+    _pythia_card_path = Path(config.pythia["card"])
     if not _pythia_card_path.exists():
         log.error(f"{_pythia_card_path} does not exist.")
         sys.exit(1)
@@ -25,14 +32,14 @@ def generate_mg5config(config):
 
     # Controls whether to run Pythia8 or not
     pythia_onoff = "Pythia8"
-    if config.pythia['skip']:
+    if config.pythia["skip"]:
         pythia_onoff = "OFF"
         pythia_config_path = ""
 
     substitution = dict(
-        ecms=float(config.madgraph['ecms']) / 2,
-        nevents=int(config.madgraph['nevents']),
-        iseed=int(config.madgraph['seed']),
+        ecms=float(config.madgraph["ecms"]) / 2,
+        nevents=int(config.madgraph["nevents"]),
+        iseed=int(config.madgraph["seed"]),
     )
 
     masses = config.madgraph.get("masses", {})
@@ -46,7 +53,7 @@ def generate_mg5config(config):
         log.info(f"    ${key} = {value}")
 
     # Update the param card
-    param_card_path = Path(config.madgraph['paramcard']).resolve()
+    param_card_path = Path(config.madgraph["paramcard"]).resolve()
     new_param_card_path = output_path.joinpath(param_card_path.name)
     log.info(f"Param Card: {new_param_card_path}")
 
@@ -55,7 +62,10 @@ def generate_mg5config(config):
     )
 
     # Update the run card
-    run_card_path = Path(config.madgraph['run']['card']).resolve()
+    run_card_path = Path(config.madgraph["run"]["card"]).resolve()
+    if OLD_VERSION:
+        log.warning("Changing the run card due to old madgraph version.")
+        run_card_path = run_card_path.parent.joinpath("default_LO_oldformat.dat")
     new_run_card_path = output_path.joinpath(run_card_path.name)
     log.info(f"Run Card: {new_run_card_path}")
 
@@ -66,7 +76,7 @@ def generate_mg5config(config):
 
     # -- now specific opts.  may want to reverse this order at some point, and do the specific before global.
     # Note: this will only work with options in the run card that contain a "!" in the line, indicating a comment at the end of the line.
-    run_options = config.madgraph['run'].get("options", {})
+    run_options = config.madgraph["run"].get("options", {})
 
     pattern = re.compile(
         r"^\s*(?P<value>[^\s]+)\s*=\s*(?P<key>[a-z_0-9]+)\s*\!.*$", re.DOTALL
@@ -92,7 +102,7 @@ def generate_mg5config(config):
         raise KeyError(unused_keys[0])
 
     # Copy the proc card
-    proc_card_path = Path(config.madgraph['proc']['card']).resolve()
+    proc_card_path = Path(config.madgraph["proc"]["card"]).resolve()
     new_proc_card_path = output_path.joinpath(proc_card_path.name)
     log.info(f"Process Card: {new_proc_card_path}")
 
@@ -103,10 +113,10 @@ def generate_mg5config(config):
     log.info(f"MadGraph Config: {mgconfig_card_path}")
 
     # Figure out the run_mode.  0=single core, 1=cluster, 2=multicore.
-    if config.madgraph['batch']:
+    if config.madgraph["batch"]:
         run_mode = "set run_mode 0"  # we don't have MadGraph launch cluster jobs for us, we handle that ourselves.
-    elif int(config.madgraph['cores']) > 0:
-        run_mode = "set run_mode 2\nset nb_core %d" % int(config.madgraph['cores'])
+    elif int(config.madgraph["cores"]) > 0:
+        run_mode = "set run_mode 2\nset nb_core %d" % int(config.madgraph["cores"])
     else:
         run_mode = "set run_mode 2\nset nb_core %d" % int(
             multiprocessing.cpu_count() / 2
@@ -115,7 +125,7 @@ def generate_mg5config(config):
     # figure out if running with madspin or not, and if so, put the card in the right place
     madspin_onoff = "OFF"
     madspin_config_path = ""
-    if not config.madspin['skip']:
+    if not config.madspin["skip"]:
         # Copy the madspin card
         madspin_card_path = Path(config.madspin.card).resolve()
         new_madspin_card_path = output_path.joinpath("madspin_card.dat")
@@ -137,9 +147,7 @@ reweight=OFF
 set iseed {config.madgraph['seed']}
 done
 """
-
-    old_versions = ["2.4.3", "2.3.3"]
-    if any(version in config.madgraph['version'] for version in old_versions):
+    if OLD_VERSION:
         mg5config = f"""
 {run_mode}
 launch PROC_madgraph
