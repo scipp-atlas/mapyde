8e5c5f789c52964fb741084fff626a2a7e2deaf6
Mike Hance <michael.hance@cern.ch>
2022-07-01 15:26:21 -0700

    adding runners

--- src/mapyde/runner.py
+++ src/mapyde/runner.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
 import typing as T
 
-def madgraph(config: dict):
+def run_madgraph(config: dict):
     #./test/wrapper_mgpy.py config_file
     pass
 
-def delphes(config: dict):
+def run_delphes(config: dict):
     #./test/wrapper_delphes.py config_file
     pass
 
-def ana(config: dict):
+def run_ana(config: dict):
     # recompute XS, override XS if needed,
     # add XS to config, re-dump config file
     # probably want to move this to wrapper_ana.py script
@@ -17,7 +17,7 @@ def ana(config: dict):
     #./test/wrapper_ana.py config_file
     pass
 
-def pyhf(config: dict):
+def run_pyhf(config: dict):
     #./test/wrapper_pyhf.py config_file
     pass
 
