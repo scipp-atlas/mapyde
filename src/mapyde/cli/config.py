fcbabe354778ebfd153a8ffc8468587418398af8
Giordon Stark <kratsg@gmail.com>
2022-07-01 15:16:39 -0700

    move json up

--- src/mapyde/cli/config.py
+++ src/mapyde/cli/config.py
@@ -1,5 +1,6 @@
 import typer
 from mapyde.utils import load_config, build_config
+import json
 
 app = typer.Typer()
 
@@ -8,7 +9,6 @@ def parse(filename: str):
     user = load_config(filename)
     config = build_config(user)
 
-    import json
     typer.echo(json.dumps(config, indent=4))
 
 @app.command()
