01e1cfc0d0a0581a791366fe7ac1ed675bb1bd7e
Giordon Stark <kratsg@gmail.com>
2022-07-01 15:16:05 -0700

    add infrastructure

---
+++ src/mapyde/cli/run.py
@@ -0,0 +1,23 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def all():
+  pass
+
+@app.command()
+def madgraph():
+  pass
+
+@app.command()
+def delphes():
+  pass
+
+@app.command()
+def analysis():
+  pass
+
+@app.command()
+def pyhf():
+  pass
