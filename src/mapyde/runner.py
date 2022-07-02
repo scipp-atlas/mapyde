8d7e6a2652d45e606ed536296203f5b7b952bbd3
Giordon Stark <kratsg@gmail.com>
2022-07-01 17:01:05 -0700

    ok?

--- src/mapyde/runner.py
+++ src/mapyde/runner.py
@@ -1,10 +1,35 @@
 from __future__ import annotations
 import typing as T
 
+from mapyde.backends import madgraph
+import os
+
+import docker
+
+client = docker.from_env()
+
 
 def run_madgraph(config: dict):
     # ./test/wrapper_mgpy.py config_file
-    pass
+    madgraph.generate_mg5config(config)
+
+    image = f"ghcr.io/scipp-atlas/mario-mapyde/{config.madgraph.version}"
+    command = "mg5_aMC /data/run.mg5 && rsync -a PROC_madgraph /data/madgraph"
+    options = {
+        "name": f"{config.base.output}__mgpy",
+        "remove": True,
+        "user": os.geteuid(),
+        "group_add": [os.getegid()],
+        "mounts": [
+            docker.types.Mount(Path(config.base.path).joinpath("cards"), "/cards"),
+            docker.types.Mount(
+                Path(config.base.path).joinpath(config.base.output), "/data"
+            ),
+        ],
+        "working_dir": "/tmp",
+    }
+
+    client.containers.run(image, command, options)
 
 
 def run_delphes(config: dict):
