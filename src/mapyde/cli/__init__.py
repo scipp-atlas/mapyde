c1b78b77b500a7aee13337bc2e4a1794a152dca9
Giordon Stark <kratsg@gmail.com>
2022-07-12 18:51:13 -0700

    add functionality to CLI for getting the prefix and version

--- src/mapyde/cli/__init__.py
+++ src/mapyde/cli/__init__.py
@@ -3,10 +3,47 @@ Top-level entrypoint for the command line interface.
 """
 from __future__ import annotations
 
+from enum import Enum
+
 import typer
 
+import mapyde
 from mapyde.cli import config, run
 
 app = typer.Typer()
 app.add_typer(config.app, name="config")
 app.add_typer(run.app, name="run")
+
+
+class Prefix(str, Enum):
+    """
+    Enum for allowed options for printing path prefixes
+    """
+
+    DATA = "data"
+    CARDS = "cards"
+    TEMPLATES = "templates"
+
+
+@app.callback(invoke_without_command=True)
+def main(
+    version: bool = typer.Option(False, "--version", help="Print the current version."),
+    prefix: Prefix = typer.Option(
+        Prefix.DATA, help="Print the path prefix for data files."
+    ),
+) -> None:
+    """
+    Manage top-level options
+    """
+    if version:
+        typer.echo(f"mapyde v{mapyde.__version__}")
+        raise typer.Exit()
+    if prefix:
+        typer.echo(
+            {
+                Prefix.DATA: mapyde.data,
+                Prefix.CARDS: mapyde.cards,
+                Prefix.TEMPLATES: mapyde.templates,
+            }[prefix].resolve()
+        )
+        raise typer.Exit()
