6030af41ea26c9531e7c96b918e094b3d52c93f8
Giordon Stark <kratsg@gmail.com>
2022-07-12 18:16:05 -0700

    migrate cards over into the package

--- src/mapyde/__init__.py
+++ src/mapyde/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2022 Giordon Stark. All rights reserved.
+Copyright (c) 2022 Michael Hance and Giordon Stark. All rights reserved.
 
 mario-mapyde: A great package.
 """
@@ -7,6 +7,19 @@ mario-mapyde: A great package.
 
 from __future__ import annotations
 
-from _version import __version__
+import sys
 
-__all__ = ("__version__",)
+from mapyde._version import __version__
+
+# importlib.resources.as_file wasn't added until Python 3.9
+# c.f. https://docs.python.org/3.9/library/importlib.html#importlib.resources.as_file
+if sys.version_info >= (3, 9):
+    from importlib import resources
+else:
+    import importlib_resources as resources
+
+data = resources.files("mapyde") / "data"
+cards = data / "cards"
+templates = data / "templates"
+
+__all__ = ("__version__", "data", "cards", "templates")
